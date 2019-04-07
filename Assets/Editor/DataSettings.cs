f8d1f5a2ec058f3a45ecbfc7cc8515255d47b110
s.tkachenko <s.tkachenko@pampamstudio.com>
2019-04-07 15:23:18 +0300

    Added unity project

---
+++ Assets/Editor/DataSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class DataSettings
+{
+    [MenuItem("Settings/Delete saves")]
+    public static void DeleteSaves()
+    {
+        PlayerPrefs.DeleteKey(Constants.kPrefsFilename);
+    }
+
+}
